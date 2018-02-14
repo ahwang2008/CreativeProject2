12abb9cbd0dad82c9ed3b4823d8f480a38cffae9
Austin Hwang <ahwang2008@gmail.com>
2018-02-14 11:16:37 -0700

    Added files

--- script.js
+++ script.js
@@ -5,27 +5,24 @@ $(document).ready(function() {
       var value = $("#weatherInput").val();
       console.log(value);
       
-      var myurl= "http://food2fork.com/api/search?key=24c902e5c7e30bb9388990c1b06a709e&q=" + value;
+      var myurl= "http://api.yummly.com/v1/api/recipes?_app_id=84595f8e&_app_key=256cc9c10912cab72e895321f2477a8f&q=" + value;
         $.ajax({
         url : myurl,
         dataType : "json",
         success : function(json) {
           console.log(json);
-          /*
           var results = "";
-          results += '<h2>Weather in ' + json.name + "</h2>";
-          for (var i=0; i<json.weather.length; i++) {
-            results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-          }
-          results += '<h2>' + json.main.temp + " &deg;F</h2>"
-          results += "<p>"
-          for (var i=0; i<json.weather.length; i++) {
-            results += json.weather[i].description
-            if (i !== json.weather.length - 1)
-            results += ", "
+          results += 'Yummy results for ' + value;
+          for (var i=0; i<json.matches.length; i++) {
+            results += '<b>'+json.matches[i].recipeName + "</p></b>";
+            results += "Ingredients: </p>";
+            for(var j=0; j<json.matches[i].ingredients.length; j++){
+                results += "    -"+json.matches[i].ingredients[j] + "</p>";
+            }
+            results += "</p>"
           }
           results += "</p>";
-          $("#weatherResults").html(results);*/
+          $("#weatherResults").html(results);
         }
       });
     });
